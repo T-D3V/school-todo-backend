d724c88b3b2895956b161447237c6f58fdebdd30
td3v <td3v+git@td3v.ch>
2023-12-19 09:12:47 +0000

    feat: auth: fix secret key in repo

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+import uuid
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-  SECRET_KEY = 'very_secret'
+  SECRET_KEY = os.environ.get('SECRET_KEY', uuid.uuid4)
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
