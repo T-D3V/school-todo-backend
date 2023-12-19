c77bc15e690150430b3da7f8db5d113043c2f8ee
td3v <td3v+git@td3v.ch>
2023-12-19 10:34:38 +0000

    fix: jwt auth token

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY', uuid.uuid4)
+  SECRET_KEY = os.environ.get('SECRET_KEY', uuid.uuid4().bytes)
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
